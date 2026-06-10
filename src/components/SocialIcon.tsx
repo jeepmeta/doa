5aeb48208a8f02666fd9b5bb452687bb446db608
Jeep <jeepmeta@gmail.com>
2026-06-10 15:57:07 -0400

    add src/components/SocialIcon.tsx

---
+++ src/components/SocialIcon.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const SocialIcon: React.FC = () => (
+  <a href="https://x.com/JeepMeta" target="_blank" className="text-gray-400 hover:text-[#ff2a2a] transition">𝕏</a>
+);
